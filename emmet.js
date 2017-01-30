06791647c55898769b5396ccecb6d4e8d86846a2
Lennart kats <lennart@c9.io>
2017-01-30 11:32:54 +0000

    Apply .eslintrc rules

--- emmet.js
+++ emmet.js
@@ -26,32 +26,32 @@ define(function(require, exports, module) {
             loaded = true;
             
             var keymap = {
-                expand_abbreviation: {mac: "ctrl+alt+e", win: "ctrl+alt+e"},
+                expand_abbreviation: { mac: "ctrl+alt+e", win: "ctrl+alt+e" },
                 match_pair_outward: {}, // {mac: "ctrl+d", win: "ctrl+,"},
                 match_pair_inward: {}, // {mac: "ctrl+j", win: "ctrl+shift+0"},
-                matching_pair:  {}, // {mac: "ctrl+alt+j", win: "alt+j"},
-                next_edit_point:  {}, // "alt+right",
-                prev_edit_point:  {}, // "alt+left",
+                matching_pair: {}, // {mac: "ctrl+alt+j", win: "alt+j"},
+                next_edit_point: {}, // "alt+right",
+                prev_edit_point: {}, // "alt+left",
                 toggle_comment: {}, // {mac: "command+/", win: "ctrl+/"},
-                split_join_tag:  {}, // {mac: "shift+command+'", win: "shift+ctrl+`"},
-                remove_tag:  {}, // {mac: "command+'", win: "shift+ctrl+;"},
-                evaluate_math_expression:  {mac: "shift+command+y", win: "shift+ctrl+y"},
-                increment_number_by_1:  {}, // "ctrl+up",
-                decrement_number_by_1:  {}, // "ctrl+down",
-                increment_number_by_01:  {}, // "alt+up",
-                decrement_number_by_01:  {}, // "alt+down",
-                increment_number_by_10:  {}, // {mac: "alt+command+up", win: "shift+alt+up"},
-                decrement_number_by_10:  {}, // {mac: "alt+command+down", win: "shift+alt+down"},
-                select_next_item: {mac: "shift+command+.", win: "shift+ctrl+."},
-                select_previous_item: {mac: "shift+command+,", win: "shift+ctrl+,"},
-                reflect_css_value:  {}, // {mac: "shift+command+r", win: "shift+ctrl+r"},
+                split_join_tag: {}, // {mac: "shift+command+'", win: "shift+ctrl+`"},
+                remove_tag: {}, // {mac: "command+'", win: "shift+ctrl+;"},
+                evaluate_math_expression: { mac: "shift+command+y", win: "shift+ctrl+y" },
+                increment_number_by_1: {}, // "ctrl+up",
+                decrement_number_by_1: {}, // "ctrl+down",
+                increment_number_by_01: {}, // "alt+up",
+                decrement_number_by_01: {}, // "alt+down",
+                increment_number_by_10: {}, // {mac: "alt+command+up", win: "shift+alt+up"},
+                decrement_number_by_10: {}, // {mac: "alt+command+down", win: "shift+alt+down"},
+                select_next_item: { mac: "shift+command+.", win: "shift+ctrl+." },
+                select_previous_item: { mac: "shift+command+,", win: "shift+ctrl+," },
+                reflect_css_value: {}, // {mac: "shift+command+r", win: "shift+ctrl+r"},
                 
                 // encode_decode_data_url:  {}, // {mac: "shift+ctrl+d", win: "ctrl+'"},
                 // update_image_size: {mac: "shift+ctrl+i", win: "ctrl+u"},
                 // expand_as_you_type: "ctrl+alt+enter",
                 // wrap_as_you_type: {mac: "shift+ctrl+g", win: "shift+ctrl+g"},
-                expand_abbreviation_with_tab: {mac: "Tab", win: "Tab"},
-                wrap_with_abbreviation: {mac: "shift+ctrl+a", win: "shift+ctrl+a"}
+                expand_abbreviation_with_tab: { mac: "Tab", win: "Tab" },
+                wrap_with_abbreviation: { mac: "shift+ctrl+a", win: "shift+ctrl+a" }
             };
             
             for (var i in keymap) {
